bc96fe5916c3cfb632479a74f872ceac600b4f62
Shaco <edz@edzdeMacBook-Pro.local>
2021-06-24 14:16:14 +0800

    deleted parsers

--- quarkus/src/main/java/dev/lankydan/web/library/user/UserResource.java
+++ quarkus/src/main/java/dev/lankydan/web/library/user/UserResource.java
@@ -1,2 +1,61 @@
-package dev.lankydan.web.library.user;public class UserResource {
+package dev.lankydan.web.library.user;
+
+import io.smallrye.mutiny.Uni;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.awt.*;
+import java.util.Set;
+
+@Path("/user")
+public class UserResource {
+
+    @Inject
+    @RestClient
+    UserService userService;
+
+    @GET
+    @Path("/{user_id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Set<User> getUser(@PathParam String user_id) {
+        return userService.getUser(user_id);
+    };
+
+    @GET
+    @Path("/{user_id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Uni<Set<User>> getUserAsUni(@PathParam String user_id) {
+        return userService.getUserAsUni(user_id);
+    };
+
+    @GET
+    @Path("/users")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Set<User> listUsers() {
+        return userService.listUsers();
+    };
+
+    @POST
+    @Path("/user")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Set<User> createUser(User user) {
+        return userService.createUser(user);
+    };
+
+    @PUT
+    @Path("/{user_id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Set<User> updateUser(@PathParam String user_id) {
+        return userService.updateUser(user_id);
+    };
+
+    @DELETE
+    @Path("/{user_id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Set<User> deleteUser(@PathParam String user_id) {
+        return userService.deleteUser(user_id);
+    };
 }
--- quarkus/src/main/java/dev/lankydan/web/library/user/UserService.java
+++ quarkus/src/main/java/dev/lankydan/web/library/user/UserService.java
@@ -1,2 +1,46 @@
-package dev.lankydan.web.library.user;public class UserService {
+package dev.lankydan.web.library.user;
+
+
+import io.smallrye.mutiny.Uni;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.jboss.logging.annotations.Pos;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+import javax.ws.rs.*;
+import java.util.Set;
+
+@Path("/u1")
+@RegisterRestClient
+public interface UserService {
+
+    @GET
+    @Path("/{user_id}")
+    @Produces("application/json")
+    Set<User> getUser(@PathParam String user_id);
+
+    @GET
+    @Path("/{user_id}")
+    @Produces("application/json")
+    Uni<Set<User>> getUserAsUni(@PathParam String user_id);
+
+    @GET
+    @Path("/users")
+    @Produces("application/json")
+    Set<User> listUsers();
+
+    @POST
+    @Path("/user")
+    @Produces("application/json")
+    Set<User> createUser(User user);
+
+    @PUT
+    @Path("/{user_id}")
+    @Produces("application/json")
+    Set<User> updateUser(@PathParam String user_id);
+
+    @DELETE
+    @Path("/{user_id}")
+    @Produces("application/json")
+    Set<User> deleteUser(@PathParam String user_id);
+
 }
